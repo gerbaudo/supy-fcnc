c046fe99c340f6ffe9101ccc09643da2d22927b5
gerbaudo <davide.gerbaudo@gmail.com>
2013-04-17 10:26:11 +0200

    drop iso since we want to plot it

--- calculables/muon.py
+++ calculables/muon.py
@@ -13,31 +13,25 @@ class P4(wrappedChain.calculable) :
         phis = self.source[self.phi]
         ms = self.source[self.m]
         self.value = [utils.LorentzV(P,e,p,m) for P,e,p,m in zip(pts, etas, phis, ms)]
-    
-
 #_____________________________
 class Indices(wrappedChain.calculable) :
-    def __init__(self, collection = None, ptMin = None, iso = 'RelativeIso', absEtaMax = 1000) :
+    def __init__(self, collection = None, ptMin = None, absEtaMax = 1000) :
         self.fixes = collection
         self.stash(["charge","author", "loose","P4","cb_d0_exPV","cb_z0_exPV"])
         self.ptMin = ptMin
         self.absEtaMax = absEtaMax
-        self.iso = iso.join(collection)
         self.moreName = "muon "\
-            +("pt>%.1f GeV;"%ptMin if ptMin else "")\
-            +(";|eta|<%.1f"%absEtaMax if absEtaMax<1000 else "")\
-            + iso
-
+            +("pt>%.1fGeV;"%ptMin if ptMin else "")\
+            +(";|eta|<%.1f"%absEtaMax if absEtaMax<1000 else "")
+    @property
+    def name(self) : return 'Indices'.join(self.fixes)
     def update(self,ignored) :
         self.value = []        
         p4s    = self.source[self.P4]
-        isoVars = self.source[self.iso] if self.iso else None
         for i, p4 in enumerate(p4s) :
             if self.ptMin and p4.pt() < self.ptMin : continue
             if self.absEtaMax and self.absEtaMax < abs(p4.eta()) : continue
-            if self.iso and not isoVars[i] : continue # should iso be a bool or a float (and the thres here)?
             self.value.append(i)
-            # todo: implement dist pv etc.
 #_____________________________
 class RelativeIso(wrappedChain.calculable) :
     def __init__(self, collection = None, isoMax = None, isoVar = None) :
